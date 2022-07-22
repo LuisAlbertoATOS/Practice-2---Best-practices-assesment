a6ca89595727bea99aa9949d00610d3851371cc2
Perez <luis-alberto.perez@atos.net>
2022-07-22 11:46:58 -0500

    added pagination

--- app.js
+++ app.js
@@ -2,34 +2,79 @@
 // import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 var url = "https://restcountries.com/v3.1/all";
 var tableContent = document.querySelector("#table-content");
-// console.log("hi");
+// I want this to be global
+var currPage = 0;
+var data;
 axios.get(url)
     .then(function (response) {
     // handle success
-    console.log(JSON.stringify(response.data[0].name.official));
-    console.log(typeof response.data);
-    var data = response.data;
-    // sorting
-    data.sort(function (a, b) { return b.name.official - a.name.official; });
-    data.sort(function (a, b) {
-        var fa = a.name.official.toLowerCase(), fb = b.name.official.toLowerCase();
-        if (fa < fb) {
-            return -1;
-        }
-        else {
-            return 1;
-        }
-        return 0;
-    });
-    console.log(data[0].name.official);
-    // let tag = document.createElement("div");
-    // let text = document.createTextNode("Tutorix is the best e-learning platform");
-    // tag.appendChild(text);
-    // tableContent.appendChild(tag);
-    data.forEach(function (country) {
-        tableContent.insertAdjacentHTML('beforeend', "<div class=\"table-row\">\n              <p id='name'>" + country.name.official + "</p>\n              <p id='capitals'>" + country.capital + "</p>\n              <p id='region'>" + country.region + "</p>\n              <p id='population'>" + country.population + "</p>\n              <p id='flag'>" + country.flag + "</p>\n            </div>");
-    });
+    data = response.data;
+    sortCountries("ASC");
+    displayCountries(currPage);
 })["catch"](function (error) {
     // handle error
     console.log(error);
 });
+var order = "ASC";
+var btnToggle = document.querySelector("#btn-toggle");
+btnToggle.addEventListener('click', function (e) {
+    sortCountries((order === "ASC" ? "DESC" : "ASC"));
+    order = (order === "ASC" ? "DESC" : "ASC");
+    displayCountries(currPage);
+});
+function sortCountries(order) {
+    if (order === "ASC") {
+        // sorting
+        data.sort(function (a, b) { return b.name.official - a.name.official; });
+        data.sort(function (a, b) {
+            var fa = a.name.official.toLowerCase(), fb = b.name.official.toLowerCase();
+            if (fa < fb) {
+                return -1;
+            }
+            else {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    else {
+        // sorting
+        data.sort(function (a, b) { return b.name.official - a.name.official; });
+        data.sort(function (a, b) {
+            var fa = a.name.official.toLowerCase(), fb = b.name.official.toLowerCase();
+            if (fa < fb) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        });
+    }
+}
+function displayCountries(page) {
+    tableContent.innerHTML = "";
+    for (var i = page * 25; i < page * 25 + 25; i++) {
+        tableContent.insertAdjacentHTML('beforeend', "<div class=\"table-row\">\n            <p id='name'>" + data[i].name.official + "</p>\n            <p id='capitals'>" + data[i].capital + "</p>\n            <p id='region'>" + data[i].region + "</p>\n            <p id='population'>" + data[i].population + "</p>\n            <p id='flag'>" + data[i].flag + "</p>\n          </div>");
+    }
+    ;
+}
+var btnNext = document.querySelector("#next-page");
+btnNext.addEventListener('click', function (e) {
+    if (currPage < data.length / 25 - 1) {
+        currPage += 1;
+        displayCountries(currPage);
+    }
+    updatePagesButtons();
+});
+var btnPrevious = document.querySelector("#previous-page");
+btnPrevious.addEventListener('click', function (e) {
+    if (currPage >= 0) {
+        currPage -= 1;
+        displayCountries(currPage);
+    }
+    updatePagesButtons();
+});
+function updatePagesButtons() {
+    btnPrevious.disabled = (currPage === 0) ? true : false;
+    btnNext.disabled = (currPage === data.length / 25 - 1) ? true : false;
+}
--- app.ts
+++ app.ts
@@ -1,23 +1,41 @@
 // const axios = require('axios').default;
 // import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
-const url = "https://restcountries.com/v3.1/all";
-const tableContent = document.querySelector("#table-content")!;
-// console.log("hi");
+const url:string = "https://restcountries.com/v3.1/all";
+const tableContent:Element = document.querySelector("#table-content")!;
+
+// I want this to be global
+var currPage=0;
+var data:Object[]; 
 
 axios.get(url)
   .then(function (response) {
     // handle success
-    console.log(JSON.stringify(response.data[0].name.official));
-    console.log(typeof response.data)
-    let data = response.data;
+    data = response.data;
+    sortCountries("ASC");
+    displayCountries(currPage);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
 
+let order = "ASC";
+let btnToggle = document.querySelector("#btn-toggle")!;
+btnToggle.addEventListener('click', function(e){
+    sortCountries((order==="ASC"?"DESC":"ASC"));
+    order = (order==="ASC"?"DESC":"ASC")
+    displayCountries(currPage);
+});
+
+function sortCountries(order:"ASC"|"DESC"){
+  if(order === "ASC"){
     // sorting
     data.sort((a, b) => b.name.official - a.name.official);
     data.sort((a, b) => {
       let fa = a.name.official.toLowerCase(),
           fb = b.name.official.toLowerCase();
-  
+
       if (fa < fb) {
           return -1;
       } else{
@@ -25,27 +43,56 @@ axios.get(url)
       }
       return 0;
     });
-    console.log(data[0].name.official);
+  } else{
+    // sorting
+    data.sort((a, b) => b.name.official - a.name.official);
+    data.sort((a, b) => {
+      let fa = a.name.official.toLowerCase(),
+          fb = b.name.official.toLowerCase();
 
-    // let tag = document.createElement("div");
-    // let text = document.createTextNode("Tutorix is the best e-learning platform");
-    // tag.appendChild(text);
-    // tableContent.appendChild(tag);
+      if (fa < fb) {
+          return 1;
+      } else{
+          return -1;
+      }
+    });
+  }
+}
 
-    data.forEach(function(country) {
-      tableContent.insertAdjacentHTML('beforeend', 
-            `<div class="table-row">
-              <p id='name'>`+country.name.official+`</p>
-              <p id='capitals'>`+country.capital+`</p>
-              <p id='region'>`+ country.region +`</p>
-              <p id='population'>`+ country.population +`</p>
-              <p id='flag'>`+ country.flag +`</p>
-            </div>`
-        );
-    })
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
+function displayCountries(page:number){
+  tableContent.innerHTML="";
+  for(let i=page*25; i<page*25+25;i++) {
+    tableContent.insertAdjacentHTML('beforeend', 
+          `<div class="table-row">
+            <p id='name'>`+data[i].name.official+`</p>
+            <p id='capitals'>`+data[i].capital+`</p>
+            <p id='region'>`+ data[i].region +`</p>
+            <p id='population'>`+ data[i].population +`</p>
+            <p id='flag'>`+ data[i].flag +`</p>
+          </div>`
+      );
+  };
+}
 
+let btnNext = document.querySelector("#next-page")!;
+btnNext.addEventListener('click', function(e){
+  if(currPage < data.length/25-1){
+    currPage += 1;
+    displayCountries(currPage);
+  }
+  updatePagesButtons();
+});
+
+let btnPrevious = document.querySelector("#previous-page")!;
+btnPrevious.addEventListener('click', function(e){
+  if(currPage >= 0){
+    currPage -= 1;
+    displayCountries(currPage);
+  }
+  updatePagesButtons();
+});
+
+function updatePagesButtons(){
+  btnPrevious.disabled = (currPage === 0) ? true:false;
+  btnNext.disabled = (currPage === data.length/25-1) ? true:false;
+}
